ff0fb3b64e9ef05b38aa1f3d433d8667a2429bf6
Leandro Campos <lleandrodcampos@gmail.com>
2025-04-06 17:54:51 -0400

    Configuração inicial para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173, // Porta alternativa comum para Vite
+    host: '0.0.0.0', // Permite acesso de qualquer IP
+    port: 5173,
     strictPort: true,
-    open: true // Abre automaticamente no navegador
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
